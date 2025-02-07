69000e2fdbf1114dfb0dd0cc7cb0400ec490c6a2
Kiano <158245848+KaysoKal@users.noreply.github.com>
2025-02-07 14:41:09 -0600

    Update Max_Min_Of_List.py

--- Lists/Max_Min_Of_List.py
+++ Lists/Max_Min_Of_List.py
@@ -1,33 +1,54 @@
 # Find the Maximum & Minimum of a list
 
 import string
-def UserInput():
-    while True:
+def UserInput(): # create a function for user input 
+    while True: # contineous loop 
         text = input("Please enter in a list of numbers: ")
-        if any(char in string.punctuation for char in text):
+        if any(char in string.punctuation for char in text): # check for any input have a punctuation
             print("There should be no special character, Try Again")
             
-        elif any(char.isalpha() for char in text):
+        elif any(char.isalpha() for char in text): # check for any letters in text
             print("There should be no letters Try Again")
             
         else:
-            text = text.split()
-            break
+            text = text.split() # change string to list 
+            break # break the loop!
+
     return text
  
     
 
 def MaxNum(text):
-    return(text)
+    text = [int(i) for i in text] # change each string of i into a int for the list 
+    Highest_num = text[0]
+    for i in text:
+        if Highest_num < i:
+            Highest_num = i
+   
+
+        else:
+            continue
+    return(Highest_num)
 
 def MinNum(text):
-    pass
+    text = [int(i) for i in text]
+    Lowest_num = text[0]
+    for i in text:
+        if Lowest_num > i:
+            Lowest_num = i
+            
+        else:
+            continue
+    return(Lowest_num)
 
 
 def main():
     result = UserInput()
-    print(MaxNum(result))
-    print(MinNum(result))
+    print(f"The Highest number is {MaxNum(result)}")
+    print(f"The Lowest number is {MinNum(result)}")
+    
+    # u any all and punction to put in notes and how to update branch 
+    # notes on using a list cophemison and understanding the differece in (i) and [i] when usign a loop and the difference loops to use for a string and a list!!!
 
 if __name__ == "__main__":
     main()
