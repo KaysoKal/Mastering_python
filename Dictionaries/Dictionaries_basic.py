ea37a0d66e1ed9e3639a36b8389f33650087b6cd
Kiano <158245848+KaysoKal@users.noreply.github.com>
2025-03-14 11:11:09 -0500

    update dict

---
+++ Dictionaries/Dictionaries_basic.py
@@ -0,0 +1,14 @@
+"""
+Easy: Basic Dictionary Operations
+Create a dictionary that stores three favorite foods and their prices.
+Retrieve the price of one specific food item from the dictionary.
+Add a new item to the dictionary with a price.
+Update the price of an existing item in the dictionary.
+Remove an item from the dictionary.
+"""
+
+def main():
+    print("test")
+
+if __name__ == "__main__":
+    main()
