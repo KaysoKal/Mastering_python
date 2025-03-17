ecd62aece1c7e02e9df50afea460f3c93c1bc0aa
Kiano <158245848+KaysoKal@users.noreply.github.com>
2025-03-17 12:05:32 -0500

    new file created Dict_iteration

---
+++ Dictionaries/Dict_Iteration&Manipulation.py
@@ -0,0 +1,14 @@
+"""
+Medium: Dictionary Iteration & Manipulation
+Print all keys and values in the dictionary in a formatted way.
+Check if a specific item exists in the dictionary before accessing it.
+Increase all prices by 10% and update the dictionary.
+Count how many items in the dictionary have a price greater than $5.
+Merge two dictionaries containing food items and their prices. (update)
+"""
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
